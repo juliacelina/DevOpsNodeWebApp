f0bb62ecc03e99cc7cee69cad4005cc62ceb8043
juliacelina <thuerjul@students.zhaw.ch>
2025-05-12 16:30:02 +0200

    test github action

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Azure with GitHub Actions! Test 3: Azure' );
+  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Azure with GitHub Actions! Test 4: GitHub-Action' );
 });
 
 app.listen(PORT, HOST);
